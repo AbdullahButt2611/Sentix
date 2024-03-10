4739a65e04a37ad241e56baf17a06c8ee930b2e2
M. Abdullah Butt <abutt2210@gmail.com>
2024-03-10 22:51:46 +0500

    Update settings.py

--- sentix/sentix/settings.py
+++ sentix/sentix/settings.py
@@ -4,7 +4,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 # Quick-start development settings - unsuitable for production
